cdff3b0eb7b985d398c8cbb9fb41fbe3f5079dc8
jayfunk <cook.tylerj@gmail.com>
2016-04-20 13:19:57 -0400

    Cleaned up unknown annotations

--- src/data/staticData/staticMapAnnotations.js
+++ src/data/staticData/staticMapAnnotations.js
@@ -1,5 +1,4 @@
 import art from './annotations/art.json'
-import landmarks from './annotations/landmarks.json'
 import offlimits from './annotations/offlimits.json'
 import opencamping from './annotations/opencamping.json'
 import placementMap from './annotations/placementMap.json'
@@ -8,7 +7,6 @@ import tcs from './annotations/tcs.json'
 const allMapAnnotations = []
 export default allMapAnnotations.concat(
   art,
-  landmarks,
   offlimits,
   opencamping,
   placementMap,
