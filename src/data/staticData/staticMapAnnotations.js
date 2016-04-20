e7e4d66362fdd3b2675587223b1e776565794866
jayfunk <cook.tylerj@gmail.com>
2016-04-20 18:12:06 -0400

    Added effigy and temple map icons

--- src/data/staticData/staticMapAnnotations.js
+++ src/data/staticData/staticMapAnnotations.js
@@ -1,5 +1,6 @@
 import art from './annotations/art.json'
 import offlimits from './annotations/offlimits.json'
+import landmarks from './annotations/landmarks.json'
 import opencamping from './annotations/opencamping.json'
 import placementMap from './annotations/placementMap.json'
 import roads from './annotations/roads.json'
@@ -7,6 +8,7 @@ import tcs from './annotations/tcs.json'
 const allMapAnnotations = []
 export default allMapAnnotations.concat(
   art,
+  landmarks,
   offlimits,
   opencamping,
   placementMap,
