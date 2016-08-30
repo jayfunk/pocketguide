cceb18f7efc6c9912827b004e5167fcb5b12e211
jayfunk <cook.tylerj@gmail.com>
2016-08-30 13:32:51 -0400

    Reimplemented the data loading to thunk

--- src/__tests__/actionCreators.spec.js
+++ src/__tests__/actionCreators.spec.js
@@ -81,13 +81,13 @@ describe('actionCreators', () => {
         save: sinon.stub()
       }
 
-      diskStore.get.withArgs('LAST_MODIFIED_KEY').returns(Promise.resolve(new Date()))
+      diskStore.get.withArgs('LAST_MODIFIED').returns(Promise.resolve(Date.now()))
 
       const actual = loadData()(dispatch, null, {netInfo, diskStore})
 
       actual
         .then(() => {
-          expect(diskStore.get).to.be.calledWith('LAST_MODIFIED_KEY')
+          expect(diskStore.get).to.be.calledWith('LAST_MODIFIED')
           expect(dispatch).to.be.calledWith({
             type: 'data:load:complete',
             events: data.events
@@ -97,12 +97,160 @@ describe('actionCreators', () => {
         .catch(done)
     })
 
-    it('should store data from server on disk', () => {})
+    it('should store data from server on disk', (done) => {
+      const data = {
+        events: [{
+          id: 1
+        }]
+      }
 
-    it('should load data from disk if server response is 304 Not Modified', () => {})
+      const lastModified = Date.now()
 
-    it('should dispatch data:load:error when attempting to load from disk', () => {})
+      const response = {
+        ok: true,
+        json: sinon.stub().returns(Promise.resolve(data))
+      }
 
-    it('should dispatch data:load:error when server response is not "ok"', () => {})
+      const fetch = sinon.stub().returns(Promise.resolve(response))
+
+      global.fetch = fetch
+
+      const netInfo = {
+        isConnected: {
+          fetch: () => Promise.resolve(true)
+        }
+      }
+
+      const diskStore = {
+        get: sinon.stub(),
+        save: sinon.stub()
+      }
+
+      diskStore.get.withArgs('LAST_MODIFIED').returns(Promise.resolve(lastModified))
+
+      const actual = loadData()(dispatch, null, {netInfo, diskStore})
+
+      actual
+        .then(() => {
+          expect(diskStore.save).to.be.calledWith('LAST_DATA', data)
+          expect(diskStore.save).to.be.calledWith('LAST_MODIFIED')
+          done()
+        })
+        .catch(done)
+    })
+
+    it('should load data from disk if server response is 304 Not Modified', (done) => {
+      const data = {
+        events: [{
+          id: 1
+        }]
+      }
+
+      const lastModified = Date.now()
+
+      const response = {
+        ok: false,
+        status: 304,
+        json: sinon.stub().returns(Promise.resolve(data))
+      }
+
+      const fetch = sinon.stub().returns(Promise.resolve(response))
+
+      global.fetch = fetch
+
+      const netInfo = {
+        isConnected: {
+          fetch: () => Promise.resolve(true)
+        }
+      }
+
+      const diskStore = {
+        get: sinon.stub(),
+        save: sinon.stub()
+      }
+
+      diskStore.get.withArgs('LAST_MODIFIED').returns(Promise.resolve(lastModified))
+      diskStore.get.withArgs('LAST_DATA').returns(Promise.resolve(data))
+
+      const actual = loadData()(dispatch, null, {netInfo, diskStore})
+
+      actual
+        .then(() => {
+          expect(diskStore.get).to.be.calledWith('LAST_DATA')
+          done()
+        })
+        .catch(done)
+    })
+
+    it('should dispatch data:load:error when server response is not "ok" and not 304', (done) => {
+      const data = {
+        events: [{
+          id: 1
+        }]
+      }
+
+      const lastModified = Date.now()
+
+      const response = {
+        ok: false,
+        status: 500,
+        json: sinon.stub().returns(Promise.resolve(data))
+      }
+
+      const fetch = sinon.stub().returns(Promise.resolve(response))
+
+      global.fetch = fetch
+
+      const netInfo = {
+        isConnected: {
+          fetch: () => Promise.resolve(true)
+        }
+      }
+
+      const diskStore = {
+        get: sinon.stub(),
+        save: sinon.stub()
+      }
+
+      diskStore.get.withArgs('LAST_MODIFIED').returns(Promise.resolve(lastModified))
+
+      const actual = loadData()(dispatch, null, {netInfo, diskStore})
+
+      actual
+        .then(() => {
+          expect(dispatch).to.be.calledWith({
+            type: 'data:load:error',
+            errorMessage: 'Unable to load data from server. Unknown server response: 500'
+          })
+          done()
+        })
+        .catch(done)
+    })
+
+    it('should dispatch data:load:error when attempting to load from disk', (done) => {
+      const errorMessage = 'test'
+
+      const netInfo = {
+        isConnected: {
+          fetch: () => Promise.resolve(false)
+        }
+      }
+
+      const diskStore = {
+        get: sinon.stub().returns(Promise.reject(errorMessage))
+      }
+
+      const actual = loadData()(dispatch, null, {netInfo, diskStore})
+
+      actual
+        .then(done)
+        .catch((error) => {
+          expect(dispatch).to.be.calledWith({
+            type: 'data:load:error',
+            errorMessage: `Unable to load data from disk. ${error}`
+          })
+          done()
+        })
+    })
   })
 })
--- src/actionCreators.js
+++ src/actionCreators.js
@@ -4,38 +4,7 @@
 const URL = 'http://pocketguide-web-server.herokuapp.com/api/mobile'
 
 const LAST_DATA = 'LAST_DATA'
-const LAST_MODIFIED_KEY = 'LAST_MODIFIED_KEY'
-
-function loadFromDisk (dispatch, diskStore) {
-  return diskStore.get(LAST_DATA).then(data => {
-    dispatch({
-      type: 'data:load:complete',
-      events: data.events
-    })
-  })
-}
-
-function fetchData (dispatch, lastModified) {
-  return fetch(URL, {
-    timeout: 10000,
-    headers: {
-      'Last-Modified': lastModified
-    }
-  }).then(response => {
-    // const status = response.status
-    if (response.ok) {
-      return response.json()
-    }
-  }).then(data => {
-    if (data) {
-      // writeDataToDisk(data)
-      dispatch({
-        type: 'data:load:complete',
-        events: data.events
-      })
-    }
-  })
-}
+const LAST_MODIFIED = 'LAST_MODIFIED'
 
 export function loadData () {
   return (dispatch, getState, {netInfo, diskStore}) => {
@@ -47,9 +16,68 @@ export function loadData () {
       if (!isConnected) {
         return loadFromDisk(dispatch, diskStore)
       }
-      return diskStore.get(LAST_MODIFIED_KEY).then(lastModified => {
-        return fetchData(dispatch, lastModified)
+      return diskStore.get(LAST_MODIFIED).then(lastModified => {
+        return fetchData(dispatch, diskStore, lastModified)
+      }).catch(error => {
+        dispatch({
+          type: 'data:load:error',
+          errorMessage: `Unable to load data from disk. ${error}`
+        })
       })
     })
   }
+}
+
+function loadFromDisk (dispatch, diskStore) {
+  return diskStore
+    .get(LAST_DATA)
+    .then(data => {
+      dispatch({
+        type: 'data:load:complete',
+        events: data.events
+      })
+    })
+    .catch(error => {
+      dispatch({
+        type: 'data:load:error',
+        errorMessage: `Unable to load data from disk. ${error}`
+      })
+    })
+}
+
+function fetchData (dispatch, diskStore, lastModified) {
+  return fetch(URL, {
+    timeout: 10000,
+    headers: {
+      'Last-Modified': lastModified
+    }
+  }).then(response => {
+    const status = response.status
+    if (response.ok) {
+      return response.json()
+    } else if (status === 304) {
+      loadFromDisk(dispatch, diskStore)
+    } else {
+      dispatch({
+        type: 'data:load:error',
+        errorMessage: `Unable to load data from server. Unknown server response: ${response.status}`
+      })
+    }
+  }).then(data => {
+    if (data) {
+      writeToDisk(data, diskStore)
+      dispatch({
+        type: 'data:load:complete',
+        events: data.events
+      })
+    }
+  }).catch(error => {
+    console.error(error)
+    return loadFromDisk(dispatch, diskStore)
+  })
+}
+
+function writeToDisk (data, diskStore) {
+  diskStore.save(LAST_DATA, data)
+  diskStore.save(LAST_MODIFIED, Date.now())
 }
